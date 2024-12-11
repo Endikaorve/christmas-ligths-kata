b28dc9ecdd1b823229875c69289a3f778f888fa2
Endika Orube <endika.orube@biko2.com>
2024-12-11 19:30:41 +0100

    feat: implementacion

--- src/ChristmasLights.test.ts
+++ src/ChristmasLights.test.ts
@@ -1,9 +1,73 @@
 import { ChristmasLights } from "./ChristmasLights";
 
 describe("ChristmasLights", () => {
-  it("should ", () => {
-    const christmasLights = new ChristmasLights();
+  let christmasLights: ChristmasLights;
 
-    expect(christmasLights).toBeFalsy();
+  beforeEach(() => {
+    christmasLights = new ChristmasLights();
+  });
+
+  it("todas las luces comienzan apagadas", () => {
+    expect(christmasLights.getLightsOn()).toEqual(0);
+  });
+
+  it("encendemos todas las luces", () => {
+    christmasLights.turnOn({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    expect(christmasLights.getLightsOn()).toEqual(1_000_000);
+  });
+
+  it("encendemos todas las luces y apagamos la primera columna", () => {
+    christmasLights.turnOn({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    christmasLights.turnOff({
+      start: [0, 0],
+      end: [0, 999],
+    });
+
+    expect(christmasLights.getLightsOn()).toEqual(999_000);
+  });
+
+  it("encendemos todas las luces y apagamos todas las luces", () => {
+    christmasLights.turnOn({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    christmasLights.turnOff({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    expect(christmasLights.getLightsOn()).toEqual(0);
+  });
+
+  it("toggleamos todas las luces desde apagadas", () => {
+    christmasLights.toggle({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    expect(christmasLights.getLightsOn()).toEqual(1_000_000);
+  });
+
+  it("toggleamos todas las luces desde encendidas", () => {
+    christmasLights.turnOn({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    christmasLights.toggle({
+      start: [0, 0],
+      end: [999, 999],
+    });
+
+    expect(christmasLights.getLightsOn()).toEqual(0);
   });
 });
--- src/ChristmasLights.ts
+++ src/ChristmasLights.ts
@@ -1,1 +1,45 @@
-export class ChristmasLights {}
+export class ChristmasLights {
+  private grid: boolean[][] = Array.from({ length: 1000 }, () =>
+    Array(1000).fill(false)
+  );
+
+  public turnOn(area: { start: [number, number]; end: [number, number] }) {
+    const { start, end } = area;
+    const [startRow, startColumn] = start;
+    const [endRow, endColumn] = end;
+
+    for (let row = startRow; row <= endRow; row++) {
+      for (let column = startColumn; column <= endColumn; column++) {
+        this.grid[row][column] = true;
+      }
+    }
+  }
+
+  public turnOff(area: { start: [number, number]; end: [number, number] }) {
+    const { start, end } = area;
+    const [startRow, startColumn] = start;
+    const [endRow, endColumn] = end;
+
+    for (let row = startRow; row <= endRow; row++) {
+      for (let column = startColumn; column <= endColumn; column++) {
+        this.grid[row][column] = false;
+      }
+    }
+  }
+
+  public toggle(area: { start: [number, number]; end: [number, number] }) {
+    const { start, end } = area;
+    const [startRow, startColumn] = start;
+    const [endRow, endColumn] = end;
+
+    for (let row = startRow; row <= endRow; row++) {
+      for (let column = startColumn; column <= endColumn; column++) {
+        this.grid[row][column] = !this.grid[row][column];
+      }
+    }
+  }
+
+  public getLightsOn(): number {
+    return this.grid.flat().filter((lightValue) => lightValue).length;
+  }
+}
