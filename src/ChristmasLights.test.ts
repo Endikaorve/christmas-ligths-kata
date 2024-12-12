2aa5b15b4b16171ed0c8e1ef81a0a144ea3e9329
Endika Orube <endika.orube@biko2.com>
2024-12-12 16:52:52 +0100

    test: añade test integración

--- src/ChristmasLights.test.ts
+++ src/ChristmasLights.test.ts
@@ -56,6 +56,22 @@ describe("ChristmasLights", () => {
 
       expect(christmasLights.getLightsOn()).toEqual(0);
     });
+
+    it("integración", () => {
+      christmasLights.applyInstructions([
+        "turn on 887,9 through 959,629",
+        "turn on 454,398 through 844,448",
+        "turn off 539,243 through 559,965",
+        "turn off 370,819 through 676,868",
+        "turn off 145,40 through 370,997",
+        "turn off 301,3 through 808,453",
+        "turn on 351,678 through 951,908",
+        "toggle 720,196 through 897,994",
+        "toggle 831,394 through 904,860",
+      ]);
+
+      expect(christmasLights.getLightsOn()).toEqual(230022);
+    });
   });
 
   describe("brillo", () => {
